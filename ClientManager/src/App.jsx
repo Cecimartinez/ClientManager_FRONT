88f6165d838c0b61c68a92b05a475d466482f649
Ceci Martínez <98435962+Cecimartinez@users.noreply.github.com>
2023-11-08 18:09:41 -0300

    Project Created, First Commit

---
+++ ClientManager/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className='text-neutral-500 w-screen text-center font-semibold text-xl'>
+        Client Manager 
+      </div>
+
+    </>
+  )
+}
+
+export default App
